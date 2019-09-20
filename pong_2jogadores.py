8ed183d5f8819c039d1e541e02e09b3db2519224
Murillo-Acacio <murilloaccacio@gmail.com>
2019-09-20 15:33:01 -0400

    condição de vitoria

--- pong_2jogadores.py
+++ pong_2jogadores.py
@@ -1,6 +1,7 @@
 import sys
 import turtle
 import os
+import time
 
 
 def pong():
@@ -53,6 +54,14 @@ def pong():
     placar.goto(0, 180)
     placar.write("0 : 0", align="center", font=(
         "Press Start 2P", 24, "normal"))
+
+    # display de vitoria
+    vitoria = turtle.Turtle("square")
+    vitoria.speed(0)
+    vitoria.color("green")
+    vitoria.penup()
+    vitoria.hideturtle()
+    vitoria.goto(0, 0)
 
     # Variáveis de movimentação dos pjs
     p_speed = 42
@@ -170,6 +179,46 @@ def pong():
             player2.sety(180)
         if (player2.ycor() < -180):
             player2.sety(-180)
+
+        # condição de vitoria player 1
+        if (score_1 > 5 and score_1 > score_2 + 1):
+            vitoria.write("Vitoria 'P1' ", align="center", font=(
+                "Press Start 2p", 24, "normal"))
+            time.sleep(5)
+            vitoria.clear()
+            vitoria.hideturtle()
+            score_1 = 0
+            score_2 = 0
+            placar.clear()
+            placar.write("0 : 0", align="center", font=(
+                "Press Start 2P", 24, "normal"))
+            ball.goto(0, 0)
+            ball.dx *= -1
+            if (ball.dx > 0):
+                ball.dx = 0.2
+            elif (ball.dx < 0):
+                ball.dx = -0.2
+            ball.dy = 0.2
+
+        # condição de vitoria player 2
+        if (score_2 > 5 and score_2 > score_1 + 1):
+            vitoria.write("Vitoria 'P2' ", align="center", font=(
+                "Press Start 2p", 24, "normal"))
+            time.sleep(5)
+            vitoria.clear()
+            vitoria.hideturtle()
+            score_1 = 0
+            score_2 = 0
+            placar.clear()
+            placar.write("0 : 0", align="center", font=(
+                "Press Start 2P", 24, "normal"))
+            ball.goto(0, 0)
+            ball.dx *= -1
+            if (ball.dx > 0):
+                ball.dx = 0.2
+            elif (ball.dx < 0):
+                ball.dx = -0.2
+            ball.dy = 0.2
 
         # atualização da tela
         screen.update()
