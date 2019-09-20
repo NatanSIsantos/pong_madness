f1125cb267aa5a30779ce3ad4bc41ec30f125ef9
ndmf.snf19@uea.edu.br <ndmf.snf19@uea.edu.br>
2019-09-20 11:51:38 -0400

    Menu base do jogo

---
+++ menu_pong.py
@@ -0,0 +1,73 @@
+import turtle
+import sys
+import os
+
+# Interface
+screen = turtle.Screen()
+screen.title("PONG MADNESS")
+screen.bgcolor("black")
+screen.setup(720, 480)
+screen.tracer(0)
+
+# Título do jogo
+titulo = turtle.Turtle("square")
+titulo.speed(0)
+titulo.color("white")
+titulo.penup()
+titulo.hideturtle()
+titulo.goto(0, 180)
+titulo.write("PONG MADNESS", align="center",
+             font=("Press Start 2P", 24, "normal"))
+
+# Tela de seleção
+modo = turtle.Turtle("square")
+modo.speed(0)
+modo.color("white")
+modo.penup()
+modo.hideturtle()
+modo.goto(0, 50)
+modo.write("MODOS DE JOGO", align="center",
+           font=("Press Start 2P", 16, "normal"))
+
+modo = turtle.Turtle("square")
+modo.speed(0)
+modo.color("white")
+modo.penup()
+modo.hideturtle()
+modo.goto(0, 10)
+modo.write("1P", align="center",
+           font=("Press Start 2P", 16, "normal"))
+
+modo = turtle.Turtle("square")
+modo.speed(0)
+modo.color("white")
+modo.penup()
+modo.hideturtle()
+modo.goto(0, -30)
+modo.write("2P", align="center",
+           font=("Press Start 2P", 16, "normal"))
+
+# Parâmetros da seleção
+selecao = turtle.Turtle("square")
+selecao.speed(0)
+selecao.turtlesize(1.5, 6)
+selecao.color('blue')
+selecao.fillcolor('')
+selecao.penup()
+selecao.sety(25)
+
+
+def selecao_up():
+    selecao.sety(25)
+
+
+def selecao_down():
+    selecao.sety(-15)
+
+
+screen.onkeypress(selecao_up, 'Up')
+screen.onkeypress(selecao_down, 'Down')
+screen.listen()
+
+while(True):
+    screen.update()
